e141748ff57d207aeb29d12fdd292e6528521137
Daniel Werner <dwerner@kixeye.com>
2016-06-09 09:37:15 -0700

    fixing build.py

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="hiredis:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="lua:shared", pure_c=True)
     builder.run()
 
